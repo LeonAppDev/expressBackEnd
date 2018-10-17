07eff364520ad0b9579576563df4b519ebdc8cad
leon <liangren64@gmail.com>
2018-10-17 14:23:21 +1300

    Modify Dockerfile and config.js file for staging

--- backend/config.js
+++ backend/config.js
@@ -5,13 +5,14 @@ module.exports = (process.env.NODE_ENV == 'development'? {
 	dbUser:'root',
 	dbPassword:'Test11111'
 }:(process.env.NODE_ENV == 'staging'?{  
-	host:'host.docker.internal',
-    db:'nzpt_entest',
+	//host:'172.17.0.1',
+        host:'localhost', 
+        db:'nzpt_entest',
 	dbUser:'nzpt_en',
 	dbPassword:'NZPT@data'
 }:{
 	host:'host.docker.internal',
-    db:'nzpt_en',
+        db:'nzpt_en',
 	dbUser:'nzpt_en',
 	dbPassword:'NZPT@data'
 }));
