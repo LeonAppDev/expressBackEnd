899ede88c9a7f786509a31aeb5234c9228a452bd
Leon Ren <liangren64@gmail.com>
2019-03-01 14:40:35 +1300

    Add docker launch scripts

--- backend/docker-launch.sh
+++ backend/docker-launch.sh
@@ -1,4 +1,15 @@
 #!usr/bin/bash
 
+echo "Begin to execute docker launch script"
 
-echo "Hello World"
+sudo docker stop $(sudo docker ps -aq)
+
+sleep 1
+
+sudo docker build -t node-web-app .
+
+sleep 1
+
+sudo docker run --net=host -t -d node-web-app
+
+echo "End to execute  docker launch script"
