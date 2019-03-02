b310edb06e05a98b3b51a572a22bb0a5aaf8b4e6
liang Ren <liangren64@gmail.com>
2019-03-02 15:08:49 +1300

    Cleanup codebase

--- backend/config.js
+++ backend/config.js
@@ -1,23 +1,39 @@
-module.exports = (process.env.NODE_ENV == 'development'? {
-	//Use below when running on Docker
-	//host:'host.docker.internal',
-	//Use localhost when running by npm command
-	host: 'localhost',
-	db:'nzpt_entest',
-	dbUser:'root',
-	dbPassword:'Test11111',
-	secret:'shhhhhhared-secret'
-}:(process.env.NODE_ENV == 'staging'?{  
-	//host:'172.17.0.1',
-    host:'localhost', 
-    db:'nzpt_entest',
-	dbUser:'nzpt_en',
-	dbPassword:'NZPT@data',
-	secret:'1019sunnyai-secret'
-}:{
-	host:'host.docker.internal',
-    db:'nzpt_en',
-	dbUser:'nzpt_en',
-	dbPassword:'NZPT@data',
-	secret:'1019sunnyai-secret'
-}));
+module.exports = (() => {
+
+	switch(process.env.NODE_ENV)
+	{
+		case 'development': 
+		    return {
+				//Use below when running on Docker
+				//host:'host.docker.internal',
+				//Use localhost when running by npm command
+				host: 'localhost',
+				db:'nzpt_entest',
+				dbUser:'root',
+				dbPassword:'Test11111',
+				secret:'shhhhhhared-secret'
+				};
+
+		case 'staging':
+			return {  
+				//host:'172.17.0.1',
+			    host:'localhost', 
+			    db:'nzpt_entest',
+				dbUser:'nzpt_en',
+				dbPassword:'NZPT@data',
+				secret:'1019sunnyai-secret'
+			    };
+
+		case 'production':
+			return {
+				host:'host.docker.internal',
+			    db:'nzpt_en',
+				dbUser:'nzpt_en',
+				dbPassword:'NZPT@data',
+				secret:'1019sunnyai-secret'
+			   };
+
+		default: 
+		   break;      
+	}
+})();
