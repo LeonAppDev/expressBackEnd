95d3b4050b4ec680f7daffdddd04385249ba7b87
liang Ren <liangren64@gmail.com>
2019-03-11 14:50:39 +1300

    Add CORS Support to Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,12 +16,17 @@ app.use('/hightea', jwt({secret: config.secret}), highTeaController);
 app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(function (err, req, res, next) {
         
+     //Enable CORS for all mehod
+    
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if(err.name === 'UnauthorizedError')
     {
         res.status(401).send('Invalid token provided.');
     }
-
+    
     logger.error(err.name);
+    next();
 });
 
 
